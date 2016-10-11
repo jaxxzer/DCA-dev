1bb9a86029c32cea2e6d25bf0ebc280ea9e63856
Jacob Walser <jwalser90@gmail.com>
2016-10-11 14:52:02 -0400

    fix bugs in remote_flash script

--- scripts/remote_flash.sh
+++ scripts/remote_flash.sh
@@ -13,7 +13,6 @@ echo $FILENAME
 
 # copy the firmware to remote host
 scp ~/git/ArduSub/ArduSub/ArduSub-v2.px4 pi@20.0.0.20:~/$FILENAME
-ssh pi@20.0.0.20 \
 
 # quit mavproxy on remote host
 # move old firmware
@@ -21,7 +20,8 @@ ssh pi@20.0.0.20 \
 # flash new firmware
 # wait 5 seconds for pixhawk to reboot
 # restart mavproxy
-"sudo screen -X -s mavproxy quit; \
+ssh pi@20.0.0.20 \
+"sudo screen -X -S mavproxy quit; \
 mv ~/Pixhawk_FW/current/*.px4 ~/Pixhawk_FW/previous/; \
 mv $FILENAME ~/Pixhawk_FW/current/;
 python px_uploader.py --port /dev/ttyACM0 ~/Pixhawk_FW/current/$FILENAME; \
