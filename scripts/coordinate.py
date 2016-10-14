f57deef66710fc49116ff5be04a92749e8a16f41
Jacob Walser <jwalser90@gmail.com>
2016-10-13 23:36:34 -0400

    add options to coordinate interpolation script

--- scripts/coordinate.py
+++ scripts/coordinate.py
@@ -1,9 +1,18 @@
 import math
 import matplotlib.pyplot as plt
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("--in", dest="infile", help="Input file, tab separated: <index radius>")
+parser.add_option("--out", dest="outfile", default="Spiral.csv", help="Output csv file: <index, x, y>")
+parser.add_option("--scalar", dest="scalar", type="float", default=1.0, help="Arbitrary scalar applied to output coordinates")
+parser.add_option("--start-ordinate", dest="start_ordinate", type="int", default=0, help="ordinate of first point, 0,1,2,3 = +x,+y,-x,-y")
+parser.add_option("--reverse", dest="reverse", default=False, action="store_true", help="spiral clockwise")
+parser.add_option("--noplot", dest="noplot", default=False, action="store_true", help="skip plot output for faster processing")
+(options,args) = parser.parse_args()
+
 
 fig = plt.figure()
-
-#sample_indices = [185, 239, 301, 416, 531, 636, 788, 969, 1151, 1297, 1479, 1715, 1952, 2161, 2440, 2780, 3121, 2262, 3646, 4020, 4394]
 
 infile = open("samples.txt", "r")
 
@@ -18,11 +27,8 @@ for line in lines:
     sample_indices.append(int(index))
     sample_radii.append(float(radius))
 
-pi = 3.141592
+current_ordinate = options.start_ordinate
 
-starting_quadrant = 0;
-
-current_quadrant = starting_quadrant;
 current_index = sample_indices[0]
 
 outfile = open("output.csv", "w")
@@ -31,24 +37,35 @@ for i in range(1,len(sample_indices)):
    radius_0 = sample_radii[i-1]
    radius_1 = sample_radii[i]
    
-   angle_base = current_quadrant * pi/2
-   current_quadrant = (current_quadrant + 1)%4
+   angle_base = current_ordinate * math.pi/2
+   if options.reverse:
+       current_ordinate = (current_ordinate - 1)%4
+   else:
+       current_ordinate = (current_ordinate + 1)%4
 
    num_between = sample_indices[i] - sample_indices[i-1]
 
    for idx in range(0, num_between):
         print current_index
-        current_index = current_index + 1
-        current_angle = angle_base + idx*math.pi/2/num_between
-        current_radius = radius_0 + idx*(radius_1 - radius_0)/num_between
+        
 
-        x,y = current_radius*math.cos(current_angle), current_radius*math.sin(current_angle)
+        if options.reverse:
+            current_angle = angle_base - idx*math.pi/2/num_between
+        else:
+            current_angle = angle_base + idx*math.pi/2/num_between
+
+        current_radius = radius_0 + idx*(radius_1 - radius_0)/num_between
+        x,y = options.scalar*current_radius*math.cos(current_angle), options.scalar*current_radius*math.sin(current_angle)
         outfile.write(str(current_index) + ',' + str(x) + ',' + str(y) + '\n')
-        plt.scatter(x, y)
+
+        if not options.noplot:
+            plt.scatter(x, y)
+
+        current_index = current_index + 1
 
 outfile.close()
 
-while True:
+while not options.noplot:
     plt.pause(0.05)
 	
 
